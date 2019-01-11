e6d85a7efadc1f577d46de275ab13e2a05d30798
Grob Oliver <groboliver1117@gmail.com>
2019-01-11 17:53:26 +0100

    Using jwt library to get username

--- frontend/src/app/services/auth/auth.service.ts
+++ frontend/src/app/services/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Subject } from 'rxjs';
+import * as jwt_decode from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
@@ -17,19 +18,24 @@ export class AuthService {
     this.loggedIn.next(true);
   }
 
-  getToken() {
-    return sessionStorage.getItem('token');
-  }
-
-  isLoggedIn() {
-    return this.getToken() !== null;
-  }
-
   logout() {
     sessionStorage.removeItem('token');
-    localStorage.removeItem('username');
     this.loggedIn.next(false);
     this.router.navigate(['/']);
+  }
+
+  getUsername(): string {
+    try {
+      console.log(Date.now() / 1000);
+      console.log(jwt_decode(sessionStorage.getItem('token')));
+      return jwt_decode(sessionStorage.getItem('token'))['sub'];
+    } catch (error) {
+      console.log('For some reason this gives an error, not my b tho');
+    }
+  }
+
+  isLoggedIn(): boolean {
+    return this.getUsername() !== undefined;
   }
 
 }
