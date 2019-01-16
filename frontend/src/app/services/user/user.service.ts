776d8576931ddadb47d487fc8b14433720cb0320
Grob Oliver <groboliver1117@gmail.com>
2019-01-16 10:30:36 +0100

    Using the same toggling logic what is present in the backend, removed console logs

--- frontend/src/app/services/user/user.service.ts
+++ frontend/src/app/services/user/user.service.ts
@@ -19,106 +19,68 @@ import { AuthService } from '../auth/auth.service';
 })
 export class UserService {
 
-  private baseUrl = 'http://localhost:8080';
-
+  private baseUrl = 'http://localhost:8080/users';
   loginStatus = new Subject<string>();
-  user = new Subject<User>();
 
   constructor(private http: HttpClient,
               private auth: AuthService,
               private router: Router,
               private toastr: ToastrService) { }
 
-  addWholeSeries(series: Series): Observable<Series> {
-    return this.http.put<any>(
-      `${this.baseUrl}/users/${this.getUsername()}/add-series-to-watched/series/${series.id}`,
-      {'series': series.id})
+  toggleSeriesInWatched(series: Series) {
+    return this.http.put<void>(
+      `${this.baseUrl}/${this.getUsername()}/toggle-series-in-watched/series/${series.id}`,
+      {'seriesId': series.id})
       .pipe(
-        tap(_ => console.log(`Series added`)),
+        tap(() => console.log('Toggle whole series in watched episodes')),
         catchError(response => this.handleError(response))
       );
   }
 
-  removeWholeSeries(series: Series): Observable<Series> {
-    return this.http.delete<any>(
-      `${this.baseUrl}/users/${this.getUsername()}/remove-series-from-watched/series/${series.id}`)
+  toggleSingleSeason(season: Season) {
+    return this.http.put<void>(
+      `${this.baseUrl}/${this.getUsername()}/toggle-season-in-watched/season/${season.id}`,
+      {'seasonId': season.id})
       .pipe(
-        tap(_ => console.log(`Series removed`)),
+        tap(() => console.log('Toggle single season in watched episodes')),
         catchError(response => this.handleError(response))
       );
   }
 
-  addSingleSeason(season: Season): Observable<Season> {
-    return this.http.put<any>(
-      `${this.baseUrl}/users/${this.getUsername()}/add-season-to-watched/season/${season.id}`,
-      {'season': season.id})
+  toggleSingleEpisode(episode: Episode) {
+    return this.http.put<void>(
+      `${this.baseUrl}/${this.getUsername()}/toggle-episode-in-watched/episode/${episode.id}`,
+      {'episodeId': episode.id})
       .pipe(
-        tap(_ => console.log(`Season added`)),
+        tap(() => console.log('Toggle single episode in watched episodes')),
         catchError(response => this.handleError(response))
       );
   }
 
-  removeSingleSeason(season: Season): Observable<Season> {
-    return this.http.delete<any>(
-      `${this.baseUrl}/users/${this.getUsername()}/remove-season-from-watched/season/${season.id}`)
+  toggleFavourites(series: Series) {
+    return this.http.put<void>(
+      `${this.baseUrl}/${this.getUsername()}/toggle-series-in-favourites/series/${series.id}`,
+      {'seriesId': series.id})
       .pipe(
-        tap(_ => console.log('Season removed')),
+        tap(() => console.log('Toggle series in favourites')),
         catchError(response => this.handleError(response))
       );
   }
 
-  addSingleEpisode(episode: Episode): Observable<Episode> {
-    return this.http.put<any>(
-      `${this.baseUrl}/users/${this.getUsername()}/add-episode-to-watched/episode/${episode.id}`,
-      {'episode': episode.id})
+  toggleWatchlist(series: Series) {
+    return this.http.put<void>(
+      `${this.baseUrl}/${this.getUsername()}/toggle-series-in-watchlist/series/${series.id}`,
+      {'seriesId': series.id})
       .pipe(
-        tap(_ => console.log(`Episode added`)),
-        catchError(response => this.handleError(response))
-      );
-  }
-
-  removeSingleEpisode(episode: Episode): Observable<Episode> {
-    return this.http.delete<any>(
-      `${this.baseUrl}/users/${this.getUsername()}/remove-episode-from-watched/episode/${episode.id}`)
-      .pipe(
-        tap(_ => console.log(`Episode removed`)),
-        catchError(response => this.handleError(response))
-      );
-  }
-
-  addToFavourites(series: Series): Observable<Series> {
-    return this.http.put<any>(
-      `${this.baseUrl}/users/${this.getUsername()}/add-series-to-favourites/series/${series.id}`,
-      {'favourite': series.id})
-      .pipe(
-        tap(_ => console.log(`Series added to favourites`)),
-        catchError(response => this.handleError(response))
-      );
-  }
-
-  removeFromFavourites(series: Series): Observable<Series> {
-    return this.http.delete<any>(
-      `${this.baseUrl}/users/${this.getUsername()}/remove-series-from-favourites/series/${series.id}`)
-      .pipe(
-        tap(_ => console.log(`Series removed from favourites`)),
-        catchError(response => this.handleError(response))
-      );
-  }
-
-  addToWatchlist(series: Series): Observable<Series> {
-    return this.http.put<any>(
-      `${this.baseUrl}/users/${this.getUsername()}/add-series-to-watchlist/series/${series.id}`,
-      {'watchlist': series.id})
-      .pipe(
-        tap(_ => console.log(`Series added to watchlist`)),
+        tap(() => console.log('Toggle series in watchlist')),
         catchError(response => this.handleError(response))
       );
   }
 
   getUser(): Observable<User> {
-    return this.http.get<any>(`${this.baseUrl}/users/${this.getUsername()}`)
+    return this.http.get<any>(`${this.baseUrl}/${this.getUsername()}`)
       .pipe(
-        tap(_ => console.log(`Getting User`)),
+        tap(() => console.log('Getting User')),
         catchError(response => this.handleError(response))
       );
   }
@@ -127,40 +89,34 @@ export class UserService {
     return this.auth.getUsername();
   }
 
-  validateJoin(username: string, email: string, password: string, confirmPassword: string): Observable<any> {
-    if (!username.trim() || !email.trim() || !password.trim() || !confirmPassword.trim()) { return of(); }
-
-    this.http.post<any>(`${this.baseUrl}/users/join`, {
-      'username': username, 'email': email, 'password': password, 'confirmPassword': confirmPassword})
+  validateJoin(username: string, email: string, password: string, confirmPassword: string) {
+    this.http.post<void>(`${this.baseUrl}/join`,
+      {'username': username, 'email': email, 'password': password, 'confirmPassword': confirmPassword})
       .pipe(
-        tap(_ => console.log(`User join`)),
-        catchError(response => this.handleError(response))
-      ).subscribe(response => {
-        this.toastr.success('You registered successfully!');
-        console.log(response);
-    });
+        tap(() => console.log('User join')),
+        catchError(response => this.handleError(response)))
+      .subscribe(
+        () => this.toastr.success('You registered successfully!')
+      );
   }
 
-  validateLogin(username: string, password: string): Observable<User> {
-    if (!username.trim() || !password.trim()) { return of(); }
-
-    this.http.post(`${this.baseUrl}/users/login`, {'username': username, 'password': password},
+  validateLogin(username: string, password: string) {
+    this.http.post<void>(`${this.baseUrl}/login`,
+      {'username': username, 'password': password},
       { observe: 'response'})
       .pipe(
-        tap(_ => console.log(`User login, should get back User`)),
-        catchError(response => this.handleError(response))
-      ).subscribe((response: HttpResponse<any>) => {
-        console.log(response);
+        tap(() => console.log('User login')),
+        catchError(response => this.handleError(response)))
+      .subscribe((response: HttpResponse<any>) => {
         if (response.headers.get('Authorization') !== null) {
-          console.log(response.headers.get('Authorization'));
+          this.toastr.success('You logged in successfully!');
           this.auth.sendToken(response.headers.get('Authorization'));
-          this.getUser().subscribe(responseUser => {
-            this.toastr.success('You logged in successfully!');
-            console.log(responseUser);
-            return this.user.next(responseUser);
-          });
         }
-    });
+      });
+  }
+
+  logoutUser() {
+    this.auth.logout();
   }
 
   private handleError<T> (error: HttpErrorResponse, result?: T) {
@@ -172,10 +128,6 @@ export class UserService {
       this.router.navigate(['/']);
     }
     return of(result as T);
-  }
-
-  logoutUser() {
-    this.auth.logout();
   }
 
 }
