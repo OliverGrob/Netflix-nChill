2e9f50b8af5c59efce86b9f6b1e27366dc837c6c
Grob Oliver <groboliver1117@gmail.com>
2019-01-30 19:03:13 +0100

    Mini game added to trending page, with some additional design

--- frontend/src/app/components/trending/trending.component.ts
+++ frontend/src/app/components/trending/trending.component.ts
@@ -2,6 +2,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { Component, OnInit } from '@angular/core';
 
 import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
 
 import { SeriesService } from '../../services/series.service';
 import { Series } from '../../models/Series';
@@ -15,16 +16,20 @@ export class TrendingComponent implements OnInit {
 
   contentLoaded = false;
   trendingSeries: Series[];
+  randomSeries: Series;
+  userChosenSeries: Series;
 
   constructor(private seriesService: SeriesService,
               private spinner: NgxSpinnerService,
-              private sanitizer: DomSanitizer) { }
+              private sanitizer: DomSanitizer,
+              private toastr: ToastrService) { }
 
   ngOnInit() {
     this.spinner.show();
     this.seriesService.getTrendingSeries().subscribe(
       (trendingSeries: Series[]) => {
         this.trendingSeries = trendingSeries;
+        this.randomSeries = this.selectRandomSeries();
         this.contentLoaded = true;
         this.spinner.hide();
       }
@@ -33,6 +38,23 @@ export class TrendingComponent implements OnInit {
 
   sanitizeUrl(url: string) {
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+  selectRandomSeries() {
+    const randomIndex = Math.floor(Math.random() * (this.trendingSeries.length - 2)) + 2;
+
+    return this.trendingSeries[randomIndex];
+  }
+
+  onSelectAnswer(series: Series) {
+    this.userChosenSeries = series;
+  }
+
+  onGuess() {
+    if (this.userChosenSeries === this.randomSeries) this.toastr.success('That\'s correct!');
+    else this.toastr.error('Read the description again!');
+
+    this.randomSeries = this.selectRandomSeries();
   }
 
 }
